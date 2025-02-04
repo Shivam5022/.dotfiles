9970beb143ed385af35590c997c1c3238d319625
Shivam Verma <shivamverma5022@gmail.com>
2025-02-04 21:20:00 +0530

    formatter flag

--- nvim/lua/shivam/plugins/formatting.lua
+++ nvim/lua/shivam/plugins/formatting.lua
@@ -14,8 +14,13 @@ return {
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 				lua = { "stylua" },
-				python = { "isort", "black" },
+				python = { "black" },
 				cpp = { "clang-format" },
+			},
+			formatters = {
+				black = {
+					prepend_args = { "--fast" },
+				},
 			},
 			format_on_save = {
 				lsp_fallback = true,
@@ -26,12 +31,12 @@ return {
 
 		-- This will be used to format a specific selection of text
 
-		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-		--   conform.format({
-		--     lsp_fallback = true,
-		--     async = false,
-		--     timeout_ms = 1000,
-		--   })
-		-- end, { desc = "Format file or range (in visual mode)" })
+		vim.keymap.set({ "n", "v" }, "<leader>for", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
