e348f36d4fdc2b47bc783a37cbd293e10e035146
Shivam Verma <shivamverma5022@gmail.com>
2025-02-04 21:05:59 +0530

    added lazygit support in nvim

--- nvim/lua/shivam/plugins/git-integration.lua
+++ nvim/lua/shivam/plugins/git-integration.lua
@@ -1,11 +1,33 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-		vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
-		vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_buffer<CR>", {})
-	end,
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_buffer<CR>", {})
+		end,
+	},
+
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+		},
+	},
 }
