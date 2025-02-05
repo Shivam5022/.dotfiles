5a9be2787498f07af84c3a5063001bc809fbb088
Shivam Verma <shivamverma5022@gmail.com>
2025-02-05 21:45:12 +0530

    added new colorscheme

--- nvim/lua/shivam/plugins/colorscheme.lua
+++ nvim/lua/shivam/plugins/colorscheme.lua
@@ -1,22 +1,36 @@
+function ColorMyPencils(color)
+	color = color or "carbonfox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+	-- this is used for changing color of highlight
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#52463d" })
+end
+
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			disable_background = true,
-			styles = {
-				italic = false,
-			},
-		})
-		vim.cmd("colorscheme rose-pine")
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
+			ColorMyPencils("rose-pine")
+		end,
+	},
 
-		-- Set transparency for all relevant highlight groups
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-
-		vim.api.nvim_set_hl(0, "Visual", { bg = "#52463d" }) -- Change to your preferred color
-	end,
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	config = function()
+	-- 		ColorMyPencils("carbonfox")
+	-- 	end,
+	-- },
 }
