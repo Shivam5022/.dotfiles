09d5a270f3609e1843d8cabb048927ca73d2ee63
Shivam Verma <shivamverma5022@gmail.com>
2025-02-19 22:20:36 +0530

    added notes floating window

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require("shivam.core")
 require("shivam.lazy")
-
+require("shivam.notes")
 Sv = function()
 	print("Hi, I am Shivam. I use nvim btw :)")
 end
---
+++ nvim/lua/shivam/notes/init.lua
@@ -0,0 +1,4 @@
+require("shivam.notes.notes").setup({
+	target_file = "notes.md",
+	global_file = "~/SV/notes.md",
+})
