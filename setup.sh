84d874a75f901c177fe31c2a4a3ee7945d8dfda5
Shivam Verma <shivamverma5022@gmail.com>
2025-02-20 00:14:45 +0530

    nits

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 ln -sf ~/.dotfiles/nvim ~/.config/
 echo "Neovim config linked successfully"
 
-ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 echo "Tmux config linked successfully"
