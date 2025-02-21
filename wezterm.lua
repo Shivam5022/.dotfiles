589615202e978566675af1fe3be0c2e13032d9e2
Shivam Verma <shivamverma5022@gmail.com>
2025-02-21 23:26:48 +0530

    added wezterm

---
+++ wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.colors = {
+	foreground = "white",
+	cursor_bg = "white",
+	cursor_border = "#47FF9C",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	-- ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	-- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+config.font = wezterm.font("JetBrainsMono Nerd Font Propo")
+config.font_size = 17
+
+-- config.window_background_image = "/path/to/wallpaper.jpg"
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.25
+config.macos_window_background_blur = 30
+
+-- and finally, return the configuration to wezterm
+return config
